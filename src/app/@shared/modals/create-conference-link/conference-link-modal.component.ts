88480c548ce50ad0dbd95b4f4d071b1ebdd13e77
prashant <ppo.opash@gmail.com>
2024-06-12 16:05:32 +0530

    Change conference call link

--- src/app/@shared/modals/create-conference-link/conference-link-modal.component.ts
+++ src/app/@shared/modals/create-conference-link/conference-link-modal.component.ts
@@ -30,7 +30,7 @@ export class ConferenceLinkComponent {
   myProp: string;
   hasDisplayedError = false;
   profileId: number;
-  originUrl = 'https://freedom.buzz/freedom-call/'
+  originUrl = 'https://nudegirls.tube/dating-call/'
   link: string = '';
   constructor(
     private spinner: NgxSpinnerService,
